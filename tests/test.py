40d58f26acc250c029d486682e3a1a4a5a97874f
mynameisvinn <vin.tang@gmail.com>
2017-06-04 09:35:55 -0400

    pylint cleanup

--- tests/test.py
+++ tests/test.py
@@ -1,38 +1,44 @@
+"""
+pymemory tests
+"""
 import unittest
 from pymemory import deep_getsizeof
 
 class InitializationTests(unittest.TestCase):
+    """
+    tests
+    """
 
     def setup(self):
         pass
 
     def test_integer(self):
         x = 5
-        assert(deep_getsizeof(x, set()) == 24)
+        assert deep_getsizeof(x, set()) == 24
 
     def test_float(self):
         x = 5.3
-        assert(deep_getsizeof(x, set()) == 24)
+        assert deep_getsizeof(x, set()) == 24
 
     def test_emptystring(self):
         x = ""
-        assert(deep_getsizeof(x, set()) == 37)        
+        assert deep_getsizeof(x, set()) == 37
 
     def test_string(self):
         x = "1234567"
-        assert(deep_getsizeof(x, set()) == 44)
+        assert deep_getsizeof(x, set()) == 44
 
     def test_emptyunicode(self):
         x = u""
-        assert(deep_getsizeof(x, set()) == 50)
+        assert deep_getsizeof(x, set()) == 50
 
     def test_unicodestring(self):
         x = u"1"
-        assert(deep_getsizeof(x, set()) == 104)
+        assert deep_getsizeof(x, set()) == 104
 
     def test_emptylist(self):
         x = []
-        assert(deep_getsizeof(x, set()) == 72)
+        assert deep_getsizeof(x, set()) == 72
 
 def main():
     unittest.main()
